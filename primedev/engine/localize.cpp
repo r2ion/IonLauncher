ddb33b1761f79f8ca8f72b6ce827d45245475475
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-01-05 01:32:49 -0800

    server localize

--- primedev/engine/localize.cpp
+++ primedev/engine/localize.cpp
@@ -2,9 +2,33 @@
 #include <vector>
 #include "localize.h"
 
-static void (*EngineClient__Localize)(
+static bool (*EngineClient__Localize)(
 	const char* key, const char** args, unsigned int num_args, char* output_buffer, unsigned int outputSize, unsigned int* a6) = nullptr;
 
+
+ADD_SQFUNC("string", Localize, "string format,...", "Localize string", ScriptContext::SERVER) {
+	
+	auto key = g_pSquirrel[context]->getstring(sqvm,1);
+	auto num_args = (sqvm->_top - sqvm->_stackbase) - 2;
+	if (num_args > 10)
+	{
+		g_pSquirrel[context]->raiseerror(sqvm, "Too many arguments");
+		return SQRESULT_ERROR;
+	}
+	std::vector<const char*> arg_list;
+	for (int i = 1; i < num_args + 1; i++)
+	{
+		arg_list.push_back(g_pSquirrel[context]->getstring(sqvm, i + 1));
+	}
+	char output_buffer[2048];
+	if (!EngineClient__Localize(key, arg_list.data(), arg_list.size(), output_buffer, sizeof(output_buffer), 0))
+	{
+		g_pSquirrel[context]->raiseerror(sqvm, "String too large for internal buffer.");
+		return SQRESULT_ERROR;
+	}
+	g_pSquirrel[context]->pushstring(sqvm, output_buffer);
+	return SQRESULT_NOTNULL;
+}
 
 std::string Localize(std::string key, ...) {
 	va_list args;
@@ -27,6 +51,7 @@ std::string Localize(std::string key, ...) {
 
 }
 
-ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", LocalizeEngine, ConCommand, (CModule module)) {
+ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", LocalizeEngine, ConCommand, (CModule module))
+{
 	EngineClient__Localize = module.Offset(0xF8CA0).RCast<decltype(EngineClient__Localize)>();
 }
