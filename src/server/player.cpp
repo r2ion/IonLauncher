34a3391a071def42eb661fe26486b1a6bbefd0a0
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-03-18 13:04:37 -0700

    restore CodeCallback_OnWeaponAttack

--- src/server/player.cpp
+++ src/server/player.cpp
@@ -2,7 +2,29 @@
 
 CPlayer*(__fastcall* UTIL_PlayerByIndex)(int playerIndex);
 
+DECLARE_MODULE(PlayerHooks)
+
+uint64_t* GlobalEntList = nullptr;
+
+const char* (*GetWeaponName)(int index);
+
+DECLARE_HOOK(PrimaryAttack, server.dll + 0x69F7C0, [](auto& hook, __int64 a1) -> void {
+	uint32_t owner_index = *(_DWORD *)(a1 + 0xEB8);
+	void* player = GlobalEntList[6 * (unsigned __int16)owner_index + 1];
+	int weapon_index = *(_DWORD *)(a1 + 0x12D8);
+	auto weapon_name = GetWeaponName(weapon_index);
+	int shotsFired = 1;
+	auto player_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)player);
+	auto weapon_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)a1);
+	auto ret = g_pSquirrel[ScriptContext::SERVER]->Call("CodeCallback_OnWeaponAttack", player_inst, weapon_inst ,weapon_name, shotsFired);
+	return hook.Original(a1);
+})
+
 ON_DLL_LOAD("server.dll", CPlayer, [](CModule module)
 {
+	DISPATCH_MODULE(PlayerHooks);
+	GetWeaponName = module.Offset(0x691300).RCast<const char* (*)(int)>();
+	GlobalEntList = *module.Offset(0xB6AB58).RCast<uint64_t**>();
 	UTIL_PlayerByIndex = module.Offset(0x26AA10).RCast<CPlayer*(__fastcall*)(int)>();
 })
+
