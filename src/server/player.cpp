a1db8bf058dae11b8138fdef8bdf81e285e19360
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-03-18 16:23:01 -0700

    fix callback

--- src/server/player.cpp
+++ src/server/player.cpp
@@ -7,7 +7,8 @@ DECLARE_MODULE(PlayerHooks)
 uint64_t* GlobalEntList = nullptr;
 
 const char* (*GetWeaponName)(int index);
-DECLARE_HOOK(PrimaryAttack, server.dll + 0x69F7C0, [](auto& hook, __int64 a1) -> void {
+
+DECLARE_HOOK(PrimaryAttack, server.dll + 0x6A0220, [](auto& hook, __int64 a1, int a2) -> bool {
 	int owner_index = *(_DWORD *)(a1 + 0xEB8);
 	void* player = (void*)GlobalEntList[6 * (unsigned __int16)owner_index + 1];
 	int weapon_index = *(_DWORD *)(a1 + 0x12D8);
@@ -15,8 +16,8 @@ DECLARE_HOOK(PrimaryAttack, server.dll + 0x69F7C0, [](auto& hook, __int64 a1) ->
 	int shotsFired = 1;
 	auto player_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)player);
 	auto weapon_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)a1);
-	auto ret = g_pSquirrel[ScriptContext::SERVER]->Call("CodeCallback_OnWeaponAttack", player_inst, weapon_inst ,weapon_name, shotsFired);
-	return hook.Original(a1);
+	g_pSquirrel[ScriptContext::SERVER]->Call("CodeCallback_OnWeaponAttack", player_inst, weapon_inst ,weapon_name, shotsFired);
+	return hook.Original(a1,a2);
 })
 
 ON_DLL_LOAD("server.dll", CPlayer, [](CModule module)
