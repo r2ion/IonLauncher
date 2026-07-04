aad3f0bef2100a0f9eb21103d71e23d4c028d7ba
Will <39478251+VITALISED@users.noreply.github.com>
2026-07-05 02:19:56 +1000

    More attack callback checks

--- src/server/player.cpp
+++ src/server/player.cpp
@@ -8,24 +8,39 @@ const char* (*GetWeaponName)(int index);
 void* (*GetWeaponOwner)(uint64_t weapon_entity);
 DECLARE_HOOK(PrimaryAttack, server.dll + 0x6A0220, [](auto& hook, __int64 a1, int a2) -> bool {
 	bool ret =  hook.Original(a1,a2);
-	if(!a1) {
+
+	if (g_pSquirrel[ScriptContext::SERVER]->m_pSQVM == nullptr || g_pSquirrel[ScriptContext::SERVER]->m_pSQVM->sqvm == nullptr)
+		return ret;
+
+	if(!a1)
+	{
 		spdlog::info("PrimaryAttack called with null weapon entity");
 		return ret;
 	}
+
 	void* player = GetWeaponOwner(a1);
-	if(!player) {
+
+	if(!player)
+	{
 		spdlog::info("PrimaryAttack called with weapon entity that has no owner");
 		return ret;
 	}
+
 	int weapon_name_index = *(_DWORD *)(a1 + 0x12D8);
-	if(weapon_name_index < 0) {
+
+	if(weapon_name_index < 0)
+	{
 		spdlog::info("PrimaryAttack called with invalid weapon name index: {}", weapon_name_index);
 		return ret;
 	}
+
 	auto weapon_name = GetWeaponName(weapon_name_index);
 	auto player_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)player);
 	auto weapon_inst = g_pSquirrel[ScriptContext::SERVER]->__sq_createscriptinstance((void*)a1);
-	g_pSquirrel[ScriptContext::SERVER]->AsyncCall("CodeCallback_OnWeaponAttack", player_inst, weapon_inst ,weapon_name, 1);
+
+	if(player_inst && weapon_inst && weapon_name)
+		g_pSquirrel[ScriptContext::SERVER]->AsyncCall("CodeCallback_OnWeaponAttack", player_inst, weapon_inst ,weapon_name, 1);
+
 	return ret;
 })
 
