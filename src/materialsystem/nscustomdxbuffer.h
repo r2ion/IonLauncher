8f4bc313503a3d60134ad707f198f1f997751066
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2026-08-11 19:15:03 -0700

    cleanup custom buffer

--- src/materialsystem/nscustomdxbuffer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Stops material-bound shader watchers and releases their shader overrides.
-// Must be called on the main thread before map materials are unloaded.
-void StopFXCAndHotReloadWatchers();
