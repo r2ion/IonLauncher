4b1d6195de3d7b0b69957b9ee36fdfbf26dc2d2b
Will <39478251+VITALISED@users.noreply.github.com>
2026-09-20 15:30:07 +1000

    fix TextEntry::Paste blocking hidden text

--- src/vgui/vgui.cpp
+++ src/vgui/vgui.cpp
@@ -7,4 +7,7 @@ ON_DLL_LOAD_CLIENT("client.dll", VGuiOffsets, [](CModule module)
 {
 	vgui_Panel_FindChildByName = module.Offset(0x759F50).RCast<vgui::Panel* (*)(vgui::Panel * thisptr, const char*, bool)>();
 	vgui_Label_SetText = module.Offset(0x780940).RCast<void (*)(vgui::Label* thisptr, const char*)>();
+
+    // fix TextEntry::Paste blocking hidden text
+    module.Offset(0x78C36D).Patch({0xEB});
 })
